00b27f84ffd7ca8d45c31817a4c8be9271f58c52
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-04-24 23:00:35 +0200

    first attemp to display content
    it is not working

--- js/menu.js
+++ js/menu.js
@@ -13,4 +13,23 @@ window.addEventListener('load',()=>{
     menu.classList.toggle("hidden");
   }
     
+    
+  fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/music/categories').then(e=>e.json()).then(buildMenu)
+    
+    function buildMenu(data){
+        let parentElement = document.querySelector('.menu ul')
+        data.forEach(item =>{
+            console.log(item);
+            let li = document.createElement('li');
+            let a = document.createAttribute('a');
+            a.textContent = item.name;
+            a.href = 'index.html?category='+ item.id;
+            
+            li.appendChild(a);
+            parentElement.appendChild(li);
+            
+        })
+    } 
 });
+    
+    
