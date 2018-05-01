3a1825a9a88281269a845a999460286309374d7d
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-05-01 23:06:14 +0200

    2nd iteration done

--- js/subpage.js
+++ js/subpage.js
@@ -3,15 +3,15 @@ let urlParams = new URLSearchParams(window.location.search);
 let id =urlParams.get('id');
 console.log('I want to get to article '+ id);
 
-fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/music/'+id).then(e =>e.json()).then(showSinglePost);
+fetch('http://wilmakorpinen.com/wp00/wp-json/wp/v2/music/'+id +'?_embed').then(e =>e.json()).then(showSinglePost);
 
 function showSinglePost(aPost){
     console.log(aPost);
 document.querySelector('#singleMusic h1').textContent = aPost.title.rendered;
-    //    document.querySelector("img").setAttribute("src", aPost._links._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url);
-    document.querySelector('#singleMusic .genre').textContent = 'genre: ' + aPost.acf.genre;
-    document.querySelector('#singleMusic span').textContent = 'price: ' + aPost.acf.price;
-    document.querySelector('#singleMusic .date').textContent = 'data: ' + aPost.acf.data;
-    document.querySelector('#singleMusic .location').textContent = 'location: '+ aPost.acf.location;
-    document.querySelector('#singleMusic .descript').innerHTML = 'description: ' + aPost.content.rendered;
+    document.querySelector("img").setAttribute("src", aPost._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url);
+    document.querySelector('#singleMusic .genre').textContent = aPost.acf.genre;
+    document.querySelector('#singleMusic span').textContent = aPost.acf.price;
+    document.querySelector('#singleMusic .date').textContent = aPost.acf.data;
+    document.querySelector('#singleMusic .location').textContent = aPost.acf.location;
+    document.querySelector('#singleMusic .descript').innerHTML = aPost.content.rendered;
 }
